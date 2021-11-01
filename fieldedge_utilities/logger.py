dc6c15df152c3cc25e05381d38ce14ce0a62b81e
geoffbrucepayne <geoff.bruce-payne@inmarsat.com>
2021-11-01 07:54:58 -0400

    module cleanup and redoc

--- fieldedge_utilities/logger.py
+++ fieldedge_utilities/logger.py
@@ -16,53 +16,32 @@ Format is:
 `2021-10-30T14:19:51.012Z,[INFO],(MainThread),main.<module>:6,This is a test`
 
 """
-import inspect
 import logging
 from logging.handlers import RotatingFileHandler
 import os
-from pathlib import Path
 import sys
 from time import gmtime
+
+from .path import clean_filename, get_caller_name
 
 FORMAT = ('%(asctime)s.%(msecs)03dZ,[%(levelname)s],(%(threadName)s),'
           '%(module)s.%(funcName)s:%(lineno)d,%(message)s')
 
 
 # Logging to STDOUT or STDERR
-class LessThanFilter(logging.Filter):
+class _LessThanFilter(logging.Filter):
     """Filters logs below a specified level for routing to a given handler."""
     def __init__(self,
                  exclusive_maximum: int = logging.WARNING,
                  name: str = None):
         if name is None:
             name = f'LessThan{logging.getLevelName(exclusive_maximum)}'
-        super(LessThanFilter, self).__init__(name)
+        super(_LessThanFilter, self).__init__(name)
         self.max_level = exclusive_maximum
 
     def filter(self, record):
         #non-zero return means we log this message
         return 1 if record.levelno < self.max_level else 0
-
-
-def clean_filename(filename: str) -> str:
-    """Adjusts relative and shorthand filenames for OS independence.
-    
-    Args:
-        filename: The full path/to/file
-    
-    Returns:
-        A clean file/path name for the current OS and directory structure.
-    """
-    if filename.startswith('$HOME/'):
-        filename = filename.replace('$HOME', str(Path.home()))
-    elif filename.startswith('~/'):
-        filename = filename.replace('~', str(Path.home()))
-    elif filename.startswith('../'):
-        mod_path = Path(__file__).parent
-        src_path = (mod_path / filename).resolve()
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        filename = os.path.join(dir_path, src_path)
-    return filename
 
 
 def get_logfile_name(logger: logging.Logger) -> str:
@@ -73,42 +52,7 @@ def get_logfile_name(logger: logging.Logger) -> str:
     return None
 
 
-def get_caller_name(depth: int = 2,
-                    mod: bool = True,
-                    cls: bool =False,
-                    mth: bool = False) -> str:
-    """Returns the name of the calling function.
-
-    Args:
-        depth: Starting depth of stack inspection.
-        mod: Include module name.
-        cls: Include class name.
-        mth: Include method name.
-    
-    Returns:
-        Name (string) including module[.class][.method]
-
-    """
-    stack = inspect.stack()
-    start = 0 + depth
-    if len(stack) < start + 1:
-        return ''
-    parent_frame = stack[start][0]
-    name = []
-    module = inspect.getmodule(parent_frame)
-    if module and mod:
-        name.append(module.__name__)
-    if cls and 'self' in parent_frame.f_locals:
-        name.append(parent_frame.f_locals['self'].__class__.__name__)
-    if mth:
-        codename = parent_frame.f_code.co_name
-        if codename != '<module>':
-            name.append(codename)
-    del parent_frame, stack
-    return '.'.join(name)
-
-
-def is_log_handler(logger: logging.Logger, handler: object) -> bool:
+def _is_log_handler(logger: logging.Logger, handler: object) -> bool:
     """Returns true if the handler is found in the logger.
     
     Args:
@@ -175,7 +119,7 @@ def get_wrapping_logger(name: str = None,
             handler_file.name = name + '_file_handler'
             handler_file.setFormatter(log_formatter)
             handler_file.setLevel(log_level)
-            if not is_log_handler(logger, handler_file):
+            if not _is_log_handler(logger, handler_file):
                 logger.addHandler(handler_file)
         except Exception as e:
             logger.exception(f'Could not create RotatingFileHandler {filename}'
@@ -185,13 +129,13 @@ def get_wrapping_logger(name: str = None,
     handler_stdout.name = name + '_stdout_handler'
     handler_stdout.setFormatter(log_formatter)
     handler_stdout.setLevel(log_level)
-    handler_stdout.addFilter(LessThanFilter(logging.WARNING))
-    if not is_log_handler(logger, handler_stdout):
+    handler_stdout.addFilter(_LessThanFilter(logging.WARNING))
+    if not _is_log_handler(logger, handler_stdout):
         logger.addHandler(handler_stdout)
     handler_stderr = logging.StreamHandler(sys.stderr)
     handler_stderr.name = name + '_stderr_handler'
     handler_stderr.setFormatter(log_formatter)
     handler_stderr.setLevel(logging.WARNING)
-    if not is_log_handler(logger, handler_stderr):
+    if not _is_log_handler(logger, handler_stderr):
         logger.addHandler(handler_stderr)
     return logger

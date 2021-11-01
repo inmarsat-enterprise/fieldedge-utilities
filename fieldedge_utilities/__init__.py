bc906ffcb90ba888147b1268b38c84527f1fb1f2
geoffbrucepayne <geoff.bruce-payne@inmarsat.com>
2021-11-01 07:36:52 -0400

    doc iteration

--- fieldedge_utilities/__init__.py
+++ fieldedge_utilities/__init__.py
@@ -5,5 +5,6 @@ This Python library contains common functionality used by many of the Inmarsat
 **FieldEdge** embedded microservices.
 
 """
+__docformat__ = 'google'
 
 __version__ = '0.1.0'
--- fieldedge_utilities/logger.py
+++ fieldedge_utilities/logger.py
@@ -4,13 +4,14 @@ Provides a common log format with a console and file handler.
 The file is a wrapping log of configurable size using `RotatingFileHandler`.
 
 Format is:
-* ISO UTC timestamp including milliseconds
-* Log level in square brackets
-* Thread name in round brackets
-* ModuleName.FunctionName:(LineNumber)
-* Message
+ - ISO UTC timestamp including milliseconds
+ - Log level in square brackets
+ - Thread name in round brackets
+ - ModuleName.FunctionName:(LineNumber)
+ - Message
 
 Example:
+
 `2021-10-30T14:19:51.012Z,[INFO],(MainThread),main.<module>:6,This is a test`
 
 """

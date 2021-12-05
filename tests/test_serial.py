7c0fd7b91e49a09fc4f08b1dcb1da44a99eaa1e8
geoffbrucepayne <geoff.bruce-payne@inmarsat.com>
2021-12-04 23:50:15 -0500

    introduce serial utilities

---
+++ tests/test_serial.py
@@ -0,0 +1,11 @@
+from fieldedge_utilities import serial
+
+
+def test_validate_basic():
+    port_exists = serial.validate_port('/dev/ttyUSB0')
+    assert port_exists == False
+
+def test_get_devices():
+    devices = serial.get_devices()
+    for device in devices:
+        assert isinstance(device, serial.SerialDevice)

a9e6a1447b05ab112850672922c16b0e6cb6ed16
geoffbrucepayne <geoff.bruce-payne@inmarsat.com>
2023-05-06 12:22:42 -0400

    removed

--- fieldedge_utilities/ip_protocols.py
+++
@@ -1,9 +0,0 @@
-"""Enumerated types for protocol analysis.
-
-*DEPRECATED*.
-Moved to ip.protocols
-
-"""
-from fieldedge_utilities.ip.protocols import KnownTcpPorts, KnownUdpPorts
-
-__all__ = ['KnownTcpPorts', 'KnownUdpPorts']
